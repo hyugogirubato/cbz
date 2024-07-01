c9540e868f57237f7306000591c8b44e2eb88bb5
Oleskii Pyskun <oleksii.pyskun@volo.global>
2024-07-01 16:47:09 +0300

    Refactoring of last commit

--- cbz/models.py
+++ cbz/models.py
@@ -1,4 +1,5 @@
-from cbz.constants import YesNo, Manga, AgeRating, Format
+from cbz.constants import YesNo, Manga, AgeRating, Format, FIELDS
+from typing import Any
 
 
 class ComicModel:
@@ -45,3 +46,37 @@ class ComicModel:
     main_character_or_team: str = ""
     review: str = ""
     _filepath: str = ""
+
+    def __init__(self, kwargs: dict):
+        """
+        Set class variables from input dictionary, check types of input and cast values to correct type
+        :param kwargs: dictionary of input data
+        """
+        for kwarg_key, kwarg_value in kwargs.items():
+            for keys, values in FIELDS:
+                if kwarg_key in keys:
+                    variable, types = values
+                    if hasattr(self, variable):
+                        setattr(self, variable, self._check_type(kwarg_key, kwarg_value, types))
+                        break
+
+    @staticmethod
+    def _check_type(key: str, value: Any, types: tuple) -> Any:
+        """
+        Check type of value, and cast this value to first type of the types
+        :param key: name of key of variable (using only for best error information)
+        :param value: value for a check
+        :param types: list of the allowed types
+        :return: value with new type
+        """
+        if not isinstance(value, types):
+            raise ValueError(
+                f"Unexpected type of {key}, got: {type(value).__name__}, expected: {[i.__name__ for i in types]}")
+        return types[0](value)
+
+    def _get(self) -> dict:
+        """
+        Create dictionary from variables by FIELDS
+        :return: dictionary with variables value
+        """
+        return {key[1]: self._check_type(value[0], getattr(self, value[0]), value[1]) for key, value in FIELDS}
