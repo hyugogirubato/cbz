23f7e37561c0f4df5aa06b65d1f1dcbafd1fcd18
hyugogirubato <65763543+hyugogirubato@users.noreply.github.com>
2025-09-17 21:16:40 +0200

    load pillow extra plugins

--- cbz/__init__.py
+++ cbz/__init__.py
@@ -1,2 +1,9 @@
 from .comic import ComicInfo
 from .page import PageInfo
+
+# Register optional Pillow plugins
+for _plugin in ('pillow_avif', 'pillow_jxl'):
+    try:
+        __import__(_plugin)
+    except ImportError:
+        pass
